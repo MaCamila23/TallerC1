1cabb884eebf62ea06fc60998a8ab11e224358ec
Camila Martinez <mcmartinez16@ucatolica.edu.co>
2026-02-25 21:07:18 -0500

    Modificar menú opción salir

--- menu.py
+++ menu.py
@@ -1,26 +1,32 @@
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicación")
-print("4. División real")
-print("5. División entera")
-print("6. Módulo")
+while True:
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División real")
+    print("5. División entera")
+    print("6. Módulo")
+    print("7. Salir")
 
-opcion = input("Seleccione una opción: ")
+    opcion = input("Seleccione una opción: ")
 
-num1 = float(input("Ingrese primer número: "))
-num2 = float(input("Ingrese segundo número: "))
+    if opcion == "7":
+        print("¡Hasta luego!")
+        break
 
-if opcion == "1":
-    print(num1 + num2)
-elif opcion == "2":
-    print(num1 - num2)
-elif opcion == "3":
-    print(num1 * num2)
-elif opcion == "4":
-    print(num1 / num2)
-elif opcion == "5":
-    print(num1 // num2)
-elif opcion == "6":
-    print(num1 % num2)
-else:
-    print("Opción no válida")
+    num1 = float(input("Ingrese primer número: "))
+    num2 = float(input("Ingrese segundo número: "))
+
+    if opcion == "1":
+        print(num1 + num2)
+    elif opcion == "2":
+        print(num1 - num2)
+    elif opcion == "3":
+        print(num1 * num2)
+    elif opcion == "4":
+        print(num1 / num2)
+    elif opcion == "5":
+        print(num1 // num2)
+    elif opcion == "6":
+        print(num1 % num2)
+    else:
+        print("Opción no válida")
